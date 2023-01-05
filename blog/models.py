4e51d3683032344d45c38e8a1c3a6ead950148c3
Adam Gilroy <adamgilroy22@gmail.com>
2023-01-05 15:21:16 +0000

    Migrate Post and Comment model to database

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     content = models.TextField()
     featred_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField()
-    created_on - models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
